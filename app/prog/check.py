11bcc9b48b2c078ad14cf0e50bc35cd44c2081bb
まつ <92625267+matsukz@users.noreply.github.com>
2023-11-23 07:31:10 +0000

    [add] #1 jsonからチェック対象を取得する

--- app/prog/check.py
+++ app/prog/check.py
@@ -1,35 +1,22 @@
 from ping3 import ping 
 import datetime
+import json
 
 now = datetime.datetime.now().strftime("%X")
 result = {}
 
 def PingCheck():
 
-    # pingを実行するIPアドレス（第4オクテット）
+    # JSONの読み込み
+    data = open("/app/prog/CheckList.json","r")
+    CheckList = json.load(data)
 
-    CheckList = {
-        "Archer AX73" : {
-            "ip": "192.168.0.1",
-            "ping": -1
-        },
-
-        "WEX-1166DHPS": {
-            "ip": "192.168.0.3",
-            "ping": -1
-        },
-
-        "Xperia XZ": {
-            "ip": "192.168.0.4",
-            "ping": -1
-        }
-    }
     for key in CheckList.keys(): #keyを取得するループ
-        for i in CheckList: # 結果を格納するループ
+        for i in CheckList: # PING結果を格納するループ
             if ping(CheckList[key]["ip"]) is False:
                 CheckList[key]["ping"] = -1
             else:
-                # CheckList[i]["ping"] = "{:.3f}".format(ping(CheckList[i]["ip"]) * 1000) + "ms"
+                # 単位をmsに設定する
                 CheckList[i]["ping"] = ping(CheckList[i]["ip"], unit = "ms")
 
     return CheckList
