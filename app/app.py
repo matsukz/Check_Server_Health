3059dc947f5f767c989be1800cf61fcc9dcccdae
まつ <92625267+matsukz@users.noreply.github.com>
2023-12-28 14:46:39 +0000

    On branch new/20231228 Changes to be committed: 	renamed:    app/prog/CheckList.json -> app/Extend/CheckList.json 	new file:   app/Extend/__pycache__/check.cpython-310.pyc 	new file:   app/Extend/__pycache__/port.cpython-310.pyc 	renamed:    app/prog/check.py -> app/Extend/check.py 	new file:   app/Extend/port.py 	modified:   app/app.py
    Changes not staged for commit:
    	modified:   .gitgnore

--- app/app.py
+++ app/app.py
@@ -2,7 +2,8 @@ from flask import Flask, redirect, render_template, request
 import os
 from ping3 import ping 
 
-from prog import check
+from Extend import check
+from Extend import port
 app = Flask(__name__)
 
 @app.route("/")
@@ -24,6 +25,13 @@ def route_api():
         ip = request.args.get("ip")
         Resp = check.free_PingCheck(ip)
         return Resp
+
+@app.route("/port")
+def route_port():
+    ip = request.args.get("ip")
+    service = request.args.get("port")
+    return port.check_ports(ip,service)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
