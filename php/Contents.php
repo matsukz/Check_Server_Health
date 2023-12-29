a7a6b270e34504a97046eee7d83395ee146ec7d1
まつ <92625267+matsukz@users.noreply.github.com>
2023-12-29 12:35:22 +0000

    ほとんど完成！

--- php/Contents.php
+++ php/Contents.php
@@ -1,14 +1,7 @@
 <?php
-    $Flask="http://192.168.0.102:5500"; //FlaskのURL
-    $IP_CFNX2="192.168.0.20";
-    $IP_Mate="192.168.0.30";
-    $IP_LXC1000="192.168.0.105";
-    $IP_LXC1010="192.168.11.5";
-    $IP_NextCloud="192.168.11.3";
-
     //ポートをチェックする関数
     function PortCheck($ip,$port){
-        global $Flask;
+        $Flask="http://192.168.0.102:5500";
         $URL = $Flask."/port?ip=".$ip."&port=".$port;
         $json = file_get_contents($URL);
         $data = json_decode($json,true);
@@ -27,92 +20,49 @@
     }
 ?>
 
-<div class="accordion" id="accordion_main">
-    <!-- メイト-->
-    <div class="accordion-item">
-        <h2 class="accordion-header" id="accordion_mate_h">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#accordion_mate_main" aria-expanded="true" aria-controls="accordion_mate_main">
-                NEC-Mate001
-            </button>
-        </h2>
-        <div id="accordion_mate_main" class="accordion-collapse collapse show" aria-labelledby="accordion_mate_h">
-            <div class="accordion-body">
+<div class="d-flex justify-content-center">
+    <div class="Container col-md-8">
 
-                <div class="row">
-                    <div class="col-6">IPアドレス</div>
-                    <div class="col-6"><div class="text-center"><?php echo $IP_Mate;?></div></div>
-                </div>
+        <div class="accordion" id="accordion_main">
+            <?php
+                $json = json_decode(file_get_contents("Client.json"),true);
+                foreach($json as $key=>$value){
+                    //クライアントの数だけ継続
+                    //ボタン生成
+                    echo '<div class="accordion-item">';
+                    echo '<h2 class="accordion-header" id="accordion_'.$json[$key]["id"].'_h">';
+                    echo '<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#accordion_'.$json[$key]["id"].'_main" aria-expanded="true" aria-controls="accordion_'.$json[$key]["id"].'_main">';
+                    echo $key;
+                    echo '</button>';
+                    echo '</h2>';
 
-                <div class="row">
-                    <div class="col-6">SSH</div>
-                    <div class="col-6">
-                        <?php echo PortCheck($IP_Mate,"22");?>
-                    </div>
-
-                    <div class="col-6">DNS</div>
-                    <div class="col-6">
-                        <?php echo PortCheck($IP_Mate,"53");?>
-                    </div>
-
-                    <div class="col-6">Lighttpd</div>
-                    <div class="col-6">
-                        <?php echo PortCheck($IP_Mate,"80");?>
-                    </div>
+                    //中身生成
+                    echo '<div id="accordion_'.$json[$key]["id"].'_main" class="accordion-collapse collapse show" aria-labelledby="accordion_'.$json[$key]["id"].'_h"';
                     
-                    <div class="col-6">OpenVPN</div>
-                    <div class="col-6">
-                        <?php echo PortCheck($IP_Mate,"443");?>
-                    </div>
-                </div>
-            </div>
+                    //中身の各行を生成
+                    echo '<div class="accordion-body">';
+
+                    // IPアドレスを取得する
+                    echo '<div class="row">';
+                    echo '<div class="col-6"><div class="text-center">IPアドレス</div></div>';
+                    echo '<div class="col-6"><div class="text-center">'.$json[$key]["IP"].'</div></div>';
+                    echo '</div>';
+
+                    //サービスとポートを取得しPortCheckに投げる
+                    echo '<div class="row">';
+                        foreach($json[$key]["Port"] as $service=>$port){
+                            echo '<div class="col-6"><div class="text-center">'.$service.'</div></div>';
+                            echo '<div class="col-6">';
+                            echo PortCheck($json[$key]["IP"],$port);
+                            echo '</div>';
+                        }
+                    echo '</div>';
+                    
+                    //divタグを閉じる
+                    echo '</div></div>';
+                
+                }
+            ?>
         </div>
     </div>
-
-    <!-- LXC1010 -->
-    <div class="accordion-item">
-        <h2 class="accordion-header" id="accordion_lxc1010_h">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#accordion_lxc1010_main" aria-expanded="true" aria-controls="accordion_lxc1010_main">
-                Proxmox LXC1010
-            </button>
-        </h2>
-        <div id="accordion_lxc1010_main" class="accordion-collapse collapse show" aria-labelledby="accordion_lxc1010_h">
-            <div class="accordion-body">
-
-                <div class="row">
-                    <div class="col-6">IPアドレス</div>
-                    <div class="col-6"><div class="text-center"><?php echo $IP_LXC1010;?></div></div>
-                </div>
-
-                <div class="row">
-                    <div class="col-6">SSH</div>
-                    <div class="col-6">
-                        <?php echo PortCheck($IP_LXC1010,"22");?>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-6">DNS</div>
-                    <div class="col-6">
-                        <?php echo PortCheck($IP_LXC1010,"53");?>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-6">HTTP</div>
-                    <div class="col-6">
-                        <?php echo PortCheck($IP_LXC1010,"80");?>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-6">Minecraft</div>
-                    <div class="col-6">
-                        <?php echo PortCheck($IP_LXC1010,"25565");?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- accordionを追加するならここから下  -->
-    
 </div>
