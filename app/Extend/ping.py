f82478bceb3169a2b78f081dfd9e36ffe2ab8a8f
まつ <92625267+matsukz@users.noreply.github.com>
2024-01-04 11:52:07 +0000

    modified:   .gitignore 	deleted:    app/Extend/check.py 	new file:   app/Extend/ping.py 	modified:   app/app.py 	modified:   php/Contents.php
    WARP越しにサービスをチェックするようにした

---
+++ app/Extend/ping.py
@@ -0,0 +1,20 @@
+from ping3 import ping 
+from datetime import datetime, timedelta, timezone
+
+def check_ping(ip,option):
+    result = {}
+    ping_result = -1
+
+    result["Result"] = ping_result
+    
+    if option == "1":
+        result["Time"] = str(datetime.now(timezone(timedelta(hours=9))))
+        result["RemoteServer"] = ip
+    else:
+        pass
+
+    ping_result = ping(ip,timeout=2,unit="ms")
+
+    result["Result"] = ping_result
+
+    return result
--- php/Contents.php
+++ php/Contents.php
@@ -1,21 +1,65 @@
 <?php
+    error_reporting(0);
+
+    $Agent = "PHPServiceChecker";
+    $options = [
+        "http" => [
+            "method" => "GET",
+            "header" => "User-Agent: $Agent\r\n"
+        ]
+    ];
+    $context = stream_context_create($options);
+
     //ポートをチェックする関数
     function PortCheck($ip,$port){
-        $Flask="http://192.168.0.102:5500";
+        $Flask = "http://192.168.0.102:5500";
         $URL = $Flask."/port?ip=".$ip."&port=".$port;
-        $json = file_get_contents($URL);
+        global $context;
+        $json = file_get_contents($URL,false,$context);
         $data = json_decode($json,true);
         if ($data == true){
             if ($data["Result"] == true){
                 //OK
-                return "<div class='text-center text-success'>ONLINE</div>";
+                return '<div class="text-success">ONLINE</div>';
             }else{
                 //NG
-                return "<div class='text-center text-danger'>OFFLINE</div>";
+                return '<div class="text-danger">OFFLINE</div>';
             }
         }else{
             //Flaskへのリクエスト失敗
-            return "<div class='text-center text-danger'>API ERROR</div>";
+            return '<div class="text-danger">API ERROR</div>';
+        }
+    }
+
+    //PINGをチェックする関数
+    function PingCheck($ip){
+        global $context;
+        $Flask = "http://192.168.0.102:5500";
+        $URL = $Flask."/ping?ip=".$ip;
+        $json = file_get_contents($URL,false,$context);
+        $data = json_decode($json,true);
+        if ($data == true){
+            if ($data["Result"] >= 0){
+                //OK
+                return '<div class="text-success">OK</div>';
+            }elseif ($data["Result"] == null){
+                //NG
+                return '<div class="text-danger">NG</div>';
+            }else{
+                return '<div class="text-danger">API ERROR</div>';
+            }
+        }
+    }
+
+    function WARPCheck($ip){
+        global $context;
+        $URL = "http://".$ip."/check_warp";
+        $json = file_get_contents($URL,false,$context);
+        $data = json_decode($json,true);
+        if ($data == true){
+            return '<div class="text-success">ONLINE</div>';
+        }else{
+            return '<div class="text-danger">OFFLINE</div>';
         }
     }
 ?>
@@ -53,10 +97,25 @@
                         foreach($json[$key]["Port"] as $service=>$port){
                             echo '<div class="col-6"><div class="text-center">'.$service.'</div></div>';
                             echo '<div class="col-6">';
-                            echo PortCheck($json[$key]["IP"],$port);
+                            echo '<div class="text-center">'.PortCheck($json[$key]["IP"],$port).'</div>';
                             echo '</div>';
                         }
                     echo '</div>';
+
+                    // WARPの稼働を調べる
+                    if (array_key_exists("WARP",$json[$key])){
+                        echo '<div class="row">';
+                        echo '<div class="col-6"><div class="text-center">WARP</div></div>';
+                        echo '<div class="col-6"><div class="text-center">'.$json[$key]["WARP"].'</div></div>';
+                        echo '</div>';
+
+                        echo '<div class="row">';
+                        echo '<div class="col-6"><div class="text-center">Nginx Service</div></div>';
+                        echo '<div class="col-6"><div class="text-center">'.WARPCheck($json[$key]["WARP"]).'</div></div>';
+                        echo '</div>';
+                    }else{
+                        // WARPの設定がないのでなにもしない
+                    }                    
                     
                     //divタグを閉じる
                     echo '</div></div>';
